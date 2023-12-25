16402020f8ac9f88bf1792a55702a4b2057ec517
sajal-madmax <77548932+sajal-madmax@users.noreply.github.com>
2023-12-25 11:30:09 +0530

    added promises in fetching area

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener("submit", (e) => {
+weatherForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const location = search.value;
@@ -11,16 +11,18 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
+  try {
+    const response = await fetch("/weather?address=" + location);
+    const data = await response.json();
+
+    if (data.error) {
+      messageOne.textContent = data.error;
+    } else {
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     }
-  );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    messageOne.textContent = "An error occurred. Please try again.";
+  }
 });
